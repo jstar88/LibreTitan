bdff07a16dbbb21ea60fdd3c25869f9d986bf515
jstar88 <covolo.n@libero.it>
2011-09-14 15:19:05 +0200

    cleaned showchangelogpage

--- upload/includes/pages/ShowChangelogPage.php
+++ upload/includes/pages/ShowChangelogPage.php
@@ -1,21 +1,24 @@
 <?php
 
-if(!defined('INSIDE')){ die(header("location:../../"));}
+if (!defined('INSIDE'))
+{
+    die(header("location:../../"));
+}
 
 function ShowChangelogPage()
 {
-	global $engine;
-    
+    global $engine;
+
     $engine->assignLangFile('CHANGELOG');
+    $story = $engine->getKey('changelog');
+    foreach ($story as $a => $b)
+    {
+        $engine->assign('version_number', $a);
+        $engine->assign('description', nl2br($b));
+        $engine->append('body', $engine->output('changelog_table', true));
+    }
 
-	$story=$engine->getKey('changelog');
-    foreach($story as $a => $b)
-	{
-		$engine->assign('version_number',$a);
-        $engine->assign('description',nl2br($b));
-        $engine->append('body',$engine->output('changelog_table',true));
-	}
-
-	return display($engine->output('changelog_body'));
+    return display($engine->output('changelog_body'));
 }
+
 ?>
