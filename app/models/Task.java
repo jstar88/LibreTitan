75f0f4964f252b8125e3ace04c64648ea0ef27c1
jstar88 <covolo.n@libero.it>
2013-02-07 19:19:20 +0100

    play

---
+++ app/models/Task.java
@@ -0,0 +1,30 @@
+package models;
+
+import java.util.*;
+import play.db.ebean.*;
+import play.data.validation.Constraints.*;
+import javax.persistence.*;
+
+@Entity
+public class Task extends Model {
+  @Id  
+  public Long id;
+  @Required
+  public String label;
+  
+  public static Model.Finder<Long,Task> find = new Model.Finder(Long.class, Task.class);
+  
+  
+  public static List<Task> all() {
+	  return find.all();
+	}
+
+	public static void create(Task task) {
+	  task.save();
+	}
+
+	public static void delete(Long id) {
+	  find.ref(id).delete();
+	}
+    
+}
